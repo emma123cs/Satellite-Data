9a4f9dc3b27947193d3535b993ff7e89e7ac37c7
NicolasRattunde <82214389+NicolasRattunde@users.noreply.github.com>
2021-05-26 16:43:44 +0200

    names updated

--- src/website.py
+++ src/website.py
@@ -15,9 +15,9 @@ import geopandas as gpd
 from flask import Response
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
-from forms import MMSI_Number
-from flask_bootstrap import Bootstrap#import geopandas as gpd
-import dataframe_image as dfi
+from forms import MMSI_Number #imports the form MMSI_number from the file forms.py
+from flask_bootstrap import Bootstrap
+import dataframe_image as dfi #only needed if 'def US_ports():' function in website.py 
 
 
 
@@ -58,15 +58,22 @@ def polymap():
     #instead of printing, we save the map as an HTML file
   map_of_ports.save("templates/Polymap.html")
 
+#def US_ports():
+    #ports_coordinates = gpd.read_file("../data/ports_us.geojson")
+    #dfi.export(ports_coordinates, "static/images/PortsCoordinates.png")
 
-#we call the function:    
+
+#we call the function(s):
+    
 polymap()
+
+#US_ports()
 
 
 #then we create the fuctions that are linked to a specific MMSI number and call them later in the index app route
 #every function below uses the 'savefig' command to create a PNG file except for the folium map which renders an HTML file
 
-def simpleGraph(MMSI):
+def shipRoute(MMSI):
                 plt.figure()
                 x = grouped.get_group(MMSI).sort_values("BaseDateTime").LAT
                 y = grouped.get_group(MMSI).sort_values("BaseDateTime").LON
@@ -74,7 +81,7 @@ def simpleGraph(MMSI):
                 plt.xlabel("LAT")
                 plt.ylabel("LON")
                 plt.title("MMSI: " + str(MMSI))
-                plt.savefig("static/images/simpleGraph.png")
+                plt.savefig("static/images/shipRoute.png")
   
 def foliumMap(MMSI):
     actual_map = folium.Map(location=[grouped.get_group(MMSI).LAT.mean(),
@@ -90,29 +97,27 @@ def foliumMap(MMSI):
     #by rerunning the code, a new template is saved 
     actual_map.save("templates/map" + str(MMSI) + ".html")
 
-def scatteredGraphGreen(MMSI):
+def sLatitudeCoordinates(MMSI):
     grouped = DATA_FRAME.groupby("MMSI")
-    #df2 = pd.to_datetime(grouped.get_group(367033570).BaseDateTime).sort_values()
     DATA_FRAME2 = grouped.get_group(int(MMSI))
     DATA_FRAME2 = DATA_FRAME2.sort_values(by = ["BaseDateTime"])
     plt.figure()
     x = pd.to_datetime(DATA_FRAME2.BaseDateTime)
     y = DATA_FRAME2.LAT
     plt.scatter(x,y,c="green")
-    plt.savefig("static/images/greenGraph.png")
+    plt.savefig("static/images/sLatitudeC.png")
 
-def scatteredGraphRed(MMSI):
+def sLongitudeCoordinates(MMSI):
     grouped = DATA_FRAME.groupby("MMSI")
-    #df2 = pd.to_datetime(grouped.get_group(367033570).BaseDateTime).sort_values()
     DATA_FRAME2 = grouped.get_group(int(MMSI))
     DATA_FRAME2 = DATA_FRAME2.sort_values(by = ["BaseDateTime"])
     plt.figure()
     x = pd.to_datetime(DATA_FRAME2.BaseDateTime)
     y = DATA_FRAME2.LON
     plt.scatter(x,y,c="red")
-    plt.savefig("static/images/redGraph.png")
+    plt.savefig("static/images/scatteredLongitudeCoordinates.png")
 
-def splineOne(MMSI):
+def sLatitude(MMSI):
     fig = plt.Figure()
     DATA_FRAME2 = grouped.get_group(MMSI)
     DATA_FRAME2 = DATA_FRAME2.sort_values(by = ["BaseDateTime"])
@@ -128,9 +133,9 @@ def splineOne(MMSI):
     ax.plot(x.to_numpy(), y, 'o', label='data')
     ax.plot(xs, cs(xs), label="S")
     ax.legend(loc='lower left', ncol=2)
-    plt.savefig("static/images/Spline.png")
+    plt.savefig("static/images/splineLatitude.png")
 
-def splineTwo(MMSI):
+def sLongitude(MMSI):
     fig = plt.Figure()
     DATA_FRAME2 = grouped.get_group(MMSI)
     DATA_FRAME2 = DATA_FRAME2.sort_values(by = ["BaseDateTime"])
@@ -146,11 +151,11 @@ def splineTwo(MMSI):
     ax.plot(x.to_numpy(), y, 'o', label='data')
     ax.plot(xs, cs(xs), label="S")
     ax.legend(loc='lower left', ncol=2)
-    plt.savefig("static/images/Spline2.png")
+    plt.savefig("static/images/splineLongitude.png")
 
 
-# below we have all the different app routes that render HTML files that display the PNGs of the graphs 
-@app.route("/", methods = ['GET', 'POST'])
+#below we have all the different app routes that render HTML files that display the PNGs of the graphs 
+@app.route("/", methods = ['GET', 'POST']) 
 def index():
     #we create an array of all unique MMSI numbers in the dataset
     numbers = DATA_FRAME.MMSI.unique()
@@ -166,12 +171,12 @@ def index():
             #empty the form field
             form.MMSI_Number.data = ""
             #we call the functions with the specific MMSI which leads to the PNGs and the folium map being created
-            simpleGraph(MMSI)
+            shipRoute(MMSI)
             foliumMap(MMSI)
-            scatteredGraphGreen(MMSI)
-            scatteredGraphRed(MMSI)
-            splineOne(MMSI)
-            splineTwo(MMSI)
+            sLatitudeCoordinates(MMSI)
+            sLongitudeCoordinates(MMSI)
+            sLatitude(MMSI)
+            sLongitude(MMSI)
             return redirect(url_for('hello_world'))
         else:
             message = "That number is not in our database."
@@ -179,27 +184,27 @@ def index():
 
 @app.route("/hello")
 def hello_world():
-    return "<p>Find the analysis of the shiproute below<br><br><a href='/map'>map</a><br><a href='/simpleGraph'>Simple Graph</a><br><a href='/redGraph'>red Graph</a><br><a href='/greenGraph'>greenGraph</a><br><a href='/splineFunction'>splineFunction</a><br><a href='/splineFunction2'>splineFunction2</a></p>"
+    return "<p>Find the analysis of the shiproute below<br><br><a href='/map'>map</a><br><a href='/shipRoute'>Ship Route</a><br><a href='/scatteredLongitudeCoordinates'>Scattered Longitude Coordinates</a><br><a href='/scatteredLatitudeCoordinates'>Scattered Latitude Coordinates</a><br><a href='/splineLatitude'>Spline Latitude</a><br><a href='/splineLongitude'>Spline Longitude</a></p>"
 
-@app.route("/simpleGraph")
-def simpleGraph1():
-    return render_template('simpleGraph.html', name = 'simpleGraph', url ="static/images/simpleGraph.png")
+@app.route("/shipRoute")
+def shipRoute1():
+    return render_template('shipRoute.html', name = 'Ship Route', url ="static/images/shipRoute.png")
 
-@app.route("/greenGraph")
-def greenGraph():
-    return render_template('greenGraph.html', name = 'greenGraph', url ="/static/images/greenGraph.png")
+@app.route("/scatteredLatitudeCoordinates")
+def scatteredLatitudeCoordinates():
+    return render_template('scatteredLatitudeCoordinates.html', name = 'Scattered Latitude Coordinates', url ="/static/images/sLatitudeC.png")
 
-@app.route("/redGraph")
-def redGraph():
-    return render_template('redGraph.html', name = 'redGraph', url ="/static/images/redGraph.png")
+@app.route("/scatteredLongitudeCoordinates")
+def scatteredLongitudeCoordinates():
+    return render_template('scatteredLongitudeCoordinates.html', name = 'Scattered Longitude Coordinates', url ="/static/images/scatteredLongitudeCoordinates.png")
 
-@app.route("/splineFunction")
-def Spline():
-    return render_template('Spline.html', name = 'Spline', url ='/static/images/Spline.png')
+@app.route("/splineLatitude")
+def splineLatitude():
+    return render_template('splineLatitude.html', name = 'Spline Latitude', url ='/static/images/splineLatitude.png')
 
-@app.route("/splineFunction2")
-def Spline2():
-    return render_template('Spline2.html', name = 'Spline2', url ='/static/images/Spline2.png')
+@app.route("/splineLongitude")
+def splineLongitude():
+    return render_template('splineLongitude.html', name = 'Spline Longitude', url ='/static/images/splineLongitude.png')
 
 #and the routes that display the general map and the specific map with the ship route
 @app.route("/map")
@@ -213,4 +218,4 @@ def poly_map():
 # and the route for the poorts coordinates 
 @app.route("/PortsPolys")
 def poly_ports():
-    return render_template('poly_ports.html', name = 'PolygonCoordinates', url ='/static/PortsCoordinates.png')
+    return render_template('poly_ports.html', name = 'Polygon Coordinates', url ='/static/PortsCoordinates.png')
